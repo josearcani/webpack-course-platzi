bab4e082f4ca7157d9037efe678543bb06e0577d
josearc <juanjose.arcani2309@gmail.com>
2021-06-30 20:29:43 -0400

    Clase 17 - Webpack modo producción - uso de clen-webpack-plugin para limpiar /dist en cada build

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -73,7 +74,8 @@ module.exports = {
         }
       ]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     minimize: true,
